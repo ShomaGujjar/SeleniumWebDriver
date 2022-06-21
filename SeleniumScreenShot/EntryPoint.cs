4e582065337a79e859801e6e29235b2106676af3
Shoma <shoma.gujjar@gmail.com>
2022-06-21 23:03:34 +0100

    Adding Screenshots

---
+++ SeleniumScreenShot/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumScreenShot
+{
+    public class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver driver = new ChromeDriver();
+            string screenShotDirectory = "Directory.GetCurrentDirectory()" + @"\screenshots";
+            driver.Navigate().GoToUrl("http://google.com");
+
+            Screenshot googleScreenshot = ((ITakesScreenshot)driver).GetScreenshot();
+
+            if (!Directory.Exists(screenShotDirectory))
+            {
+                Directory.CreateDirectory(screenShotDirectory);
+            }
+
+            googleScreenshot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshot.png", ScreenshotImageFormat.Png);
+            driver.Quit();
+        }
+
+    }
+}
